34e2573660d4167b6b8343acfe2cd187497ce1a2
Nick Rodriguez <nickbenrodriguez@gmail.com>
2018-10-11 03:11:36 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,11 +1,11 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(array, element) {
-  const newArr = array.unshift(element);
-  return newArr;
+  const newArr = array;
+  return newArr.unshift(element);
 }
 
 function addElementToEndOfArray(array, element) {
-  const newArr = array.push(element);
-  return newArr;
+  const newArr = array;
+  return newArr.push(element);
 }
